154325aa9f2faff934a00ca6295803c1726a135d
Sara <sararossmay2000@gmail.com>
2025-08-16 22:15:30 +0300

    Add MySQLServer.py into Intro_to_DB

---
+++ MySQLServer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import mysql.connector
+from mysql.connector import Error
+
+def create_database():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='555702sarA.'
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+
+    except Error as err:
+        print(f"Error: {err}")
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+if __name__ == "__main__":
+    create_database()
